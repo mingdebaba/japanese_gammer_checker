5735740bd2a57321b106ddbf4d063207fa951169
mingdebaba <694104133@qq.com>
2021-11-17 20:27:15 +0900

    enter

---
+++ japanese.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
+#sys.stdin = codecs.getreader('utf_8')(sys.stdin)
+
+
+print("こんにちは")
+print ("%s で %s " % (u"パイソン", u"自然言語処理"))
