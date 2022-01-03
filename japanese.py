e6eef09131f09a5c3834a67218b45aff9f8e5dd7
mingdebaba <694104133@qq.com>
2022-01-03 16:29:49 +0900

    main

--- japanese.py
+++ japanese.py
@@ -5,3 +5,21 @@
 
 print("こんにちは")
 print ("%s で %s " % (u"パイソン", u"自然言語処理"))
+
+import re, pprint
+def pp(obj):
+    pp = pprint.PrettyPrinter(indent=4, width=160)
+    str = pp.pformat(obj)
+    return re.sub(r"\\u([0-9a-f]{4})", lambda x: unichr(int("0x"+x.group(1),
+                                                           16)), str)
+
+data = {
+    u"スクリプト言語":
+        {u"Perl": u"パール",
+         u"Python": u"パイソン",
+         u"Ruby": u"ルビー"},
+    u"関数型言語":
+        {u"Erlang": u"アーラング",
+         u"Haskell": u"ハスケル",
+         u"Lisp": u"リスプ"}
+        }
---
+++ nltk.py
@@ -0,0 +1,6 @@
+import nltk
+from nltk.corpus.reader import *
+from nltk.corpus.reader.util import *
+from nltk.text import Text
+
+print(123)
